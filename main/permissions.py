997c4eb4aea389e91b8e1b13ec55be8694dac907
Evgeniia Pankova <jen.backulina@yandex.ru>
2023-04-24 10:51:44 +0200

    Fix error

--- main/permissions.py
+++ main/permissions.py
@@ -2,7 +2,11 @@ from rest_framework import permissions
 
 
 class DeletePermission(permissions.BasePermission):
+    """
+    Permission to allow staff members to perform DELETE requests.
+    """
+
     def has_permission(self, request, view):
-        if request.method == "DELETE" and request.user.is_staff:
-            return True
-        return False
+        if request.method == "DELETE":
+            return request.user.is_staff
+        return True
